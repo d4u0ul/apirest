41c0637dcf1f74e2eb88a211e44f9d6a9b2d7bf7
d4u0ul <ahb@ecomp.poli.br>
2021-04-16 19:33:03 -0300

    código inicial springboot

---
+++ apirest-ese/src/main/java/com/polipfc/apirestese/ApirestEseApplication.java
@@ -0,0 +1,13 @@
+package com.polipfc.apirestese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestEseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestEseApplication.class, args);
+	}
+
+}
---
+++ apirest-ese/src/test/java/com/polipfc/apirestese/ApirestEseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polipfc.apirestese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestEseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
